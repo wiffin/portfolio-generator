3d14bb7a43f74f4b0a89b8623d9ab87e30382000
wiffin <wiffinian@gmail.com>
2022-01-12 10:47:41 -0500

    added inquire

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const fs = require('fs');
-/* console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    } 
+inquirer.prompt([{
+  type: 'input',
+  name: 'name',
+  message: 'What is your name?'
+}])
+.then(answers => console.log(answers))
+/* const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-    console.log('================');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs); */
-
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-/* console.log(name, github);
-console.log(generatePage(name, github)); */
+const pageHTML = generatePage(name, github);
 
 fs.writeFile('index.html', generatePage(name, github), err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+}); */
+
